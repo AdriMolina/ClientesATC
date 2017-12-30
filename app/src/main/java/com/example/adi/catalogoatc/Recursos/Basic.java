e1e49adf1379a5e69bf5935e076fabda1b9dc2cd
AdriiMolina <ti13110051@itess.edu.mx>
2017-12-30 13:08:54 -0600

    muestra la lista

---
+++ app/src/main/java/com/example/adi/catalogoatc/Recursos/Basic.java
@@ -0,0 +1,16 @@
+package com.example.adi.catalogoatc.Recursos;
+
+/**
+ * Created by Adi on 29/12/2017.
+ */
+
+public interface Basic {
+    String SERVER = "http://192.168.1.97/";
+    String RUTA = "CatalogoATC/";
+    String HOST = "localhost";
+    String USER = "root";
+    String PASS = "";
+    String DB = "catalogoatc";
+
+
+}
